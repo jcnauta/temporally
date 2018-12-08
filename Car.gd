82294ee37e8114e92b8e1778bc2384f270a2ca12
Jelle Nauta <jc.nauta@gmail.com>
2018-12-08 13:46:27 +0100

    Switched to 2D

--- Car.gd
+++ Car.gd
@@ -1,12 +1,14 @@
-extends KinematicBody
+extends KinematicBody2D
 
 export (float) var a
-export (float) var mass
-export (float) var turnSpeed
 export (float) var maxSpeed
 
+export (float) var turnSpeed
+export (float) var maxTurn
+
+
 var speed = 0.0
-var velocity = Vector3()
+var velocity = Vector2()
 var turning = 0.0
 var alive = true
 
@@ -20,4 +22,6 @@ func _physics_process(delta):
   if not alive:
     return
   control(delta)
-  move_and_slide(velocity, Vector3(0.0, 1.0, 0.0))
+  rotation_degrees -= turning * speed * delta
+  velocity = Vector2(1.0, 0.0).rotated(rotation) * speed
+  move_and_slide(velocity)
--- Player.gd
+++ Player.gd
@@ -4,15 +4,15 @@ func control(delta):
   var tLeft = Input.is_action_pressed('turnLeft')
   var tRight = Input.is_action_pressed('turnRight')
   if tLeft:
-    turning = clamp(turning - turnSpeed * delta, -60, 60) # set and limit steering wheel rotations
+    turning = clamp(turning + turnSpeed * delta, -maxTurn, maxTurn) # set and limit steering wheel rotations
   if tRight:
-    turning = clamp(turning + turnSpeed * delta, -60, 60) # set and limit steering wheel rotations
-  turning -= 0.1 * sign(turning) * delta
+    turning = clamp(turning - turnSpeed * delta, -maxTurn, maxTurn) # set and limit steering wheel rotations
+  turning -= 2 * turning * delta
   if Input.is_action_pressed('forward'):
     speed += a * delta
     print(speed)
   if Input.is_action_pressed('back'):
     speed -= a * delta
-  rotation_degrees.y -= turning * speed * delta
+
 #  velocity.rotated(Vector3(0, 1.0, 0), turning)
   
