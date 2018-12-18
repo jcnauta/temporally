be35c94b431c5a931c79d283a24229db1e6ad1fa
Jelle Nauta <jc.nauta@gmail.com>
2018-12-19 00:06:49 +0100

    Generate borders for the track, ruling out track overlap. Place player in initial position.

--- Roadie.gd
+++ Roadie.gd
@@ -3,10 +3,7 @@ extends Node2D
 var Roadside = preload("res://Roadside.gd")
 var Delauney = preload("res://Delauney.gd")
 
-var nWps = 25
-var radius = 1000
-var roadSize = 200
-var wps = []
+var initWps = 15
 var points =  PoolVector2Array()
 var boundary = PoolVector2Array()
 var bIdxs = PoolIntArray()
@@ -14,51 +11,87 @@ var triang = PoolIntArray()
 var triangCopy = PoolIntArray()
 var counter = 0.0
 var runNr = 0
-var generate = false
+var generate = true
 var offendingPoint = null
 var offPrev = null
 var offNext = null
+var minDist
+var roadSize
+
+var burnRuns = 0
 
 func _ready():
-  var v1 = Vector2(0.0, 1.0)
-  var v2 = Vector2(1.0, 0.0)
-  print("angletest " + str(v1.angle_to(v2)))
-  print("modulotest " + str(-2 % 6))
-  createWaypoints(nWps)
-  
-#  wps = createWaypoints(nWps) # creates global wps
-#  var leftSide = wayPointsBorder(wps, "left")
-#  var rightSide = wayPointsBorder(wps, "right")
-#  get_parent().call_deferred("add_child", Roadside.new(wps))
-#  get_parent().call_deferred("add_child", Roadside.new(leftSide))
-#  get_parent().call_deferred("add_child", Roadside.new(rightSide))
+  createValidWaypoints(initWps)
+  print("points: " + str(points.size()))
+  print("bIdxs: " + str(bIdxs.size()))
+  var leftSide = wayPointsBorder(bIdxs, "left")
+  print("left: " + str(leftSide.size()))
+  var rightSide = wayPointsBorder(bIdxs, "right")
+#  get_parent().call_deferred("add_child", Roadside.new(points, bIdxs))
+  get_parent().call_deferred("add_child", Roadside.new(leftSide))
+  get_parent().call_deferred("add_child", Roadside.new(rightSide))
+  var player = $"../YSort/Player"
+  player.position = points[bIdxs[0]]
+  var towards = points[bIdxs[1]] - points[bIdxs[0]]
+  player.rotation = towards.angle()
   set_process(true)
 
-func _process(delta):
-  counter += delta
-  if counter > 1.0: #and !debugging:
-    if generate:
-      offendingPoint = null
-      offPrev = null
-      offNext = null
-      createWaypoints(nWps)
-      runNr += 1
-      update()
-      generate = false
+#func _process(delta):
+#  counter += delta
+#  if counter > 2.0:
+#    while burnRuns > 0: # for debugging, seed is the same every time.
+#      bIdxs = createWaypoints(initWps)
+#      burnRuns -= 1
+#    createValidWaypoints(initWps)
+#    else:
+#      print("good circuit after " + str(failures) + " attempts.")
+#    runNr += 1
+#    update()
+#    counter = 0.0
+
+func createValidWaypoints(initWps):
+  var isBad = true
+  var failures = -1
+  var failureLimit = 30
+  while isBad && failures < failureLimit:
+    failures += 1
+    bIdxs = createWaypoints(initWps)
+    var removedCorners = true
+    var someMoved = true
+    var confirmed = false
+    var maxMods = 100
+    var cmods = 0
+    var mmods = 0
+    while removedCorners || someMoved:
+      removedCorners = removeSharpCorners()
+      someMoved = segmentsPushPoints()
+      if removedCorners:
+        cmods += 1
+      if someMoved:
+        mmods += 1
+      if cmods > maxMods || mmods > maxMods:
+        break
+    if cmods > maxMods || mmods > maxMods:
+      print("Softening corners and pushing points was too difficult.")
+      print("cmods: " + str(cmods) + ", mmods: " + str(mmods))
+      isBad = true
     else:
-      removeSharpCorners()
-      update()
-      generate = true
-    counter = 0.0
+      isBad = circuitHasIntersections()
+  if failures == failureLimit:
+    print("Failed to create circuit, difficult parameters.")
+    get_tree().quit()
 
 # Generates the track.
 # Method based on https://stackoverflow.com/a/14266101/804318
-func createWaypoints(nWps):
-  var failure = true
-  
+func createWaypoints(initWps):
+  var side = 2500.0
+  roadSize = 300
+#  minDist = 0.5 * side / sqrt(initWps) # always possible to place point
+#  roadSize = 0.5 * minDist
+  minDist = 2 * roadSize
+  side = 2 * minDist * sqrt(initWps)
+  var failure = true  
   while failure:
-    print("new waypoints! runNr " + str(runNr))
-    var side = 1000.0
     points =  PoolVector2Array()
     boundary = PoolVector2Array()
     triang = PoolIntArray()
@@ -69,13 +102,12 @@ func createWaypoints(nWps):
       tries += 1
       # first generate the point set
       points.resize(0)
-      var minDist = 0.1 * side / sqrt(nWps) # always possible to place point
-      for i in range(nWps):
+      for i in initWps:
         # regenerate points if they are too close to another
         var tooClose = true;
         var newPoint;
         while tooClose:
-          newPoint = Vector2(randf() * side - side / 2, randf() * side - side / 2)
+          newPoint = Vector2(randf() * side + side / 2, randf() * side - side / 2)
           tooClose = false
           for j in points.size():
             if newPoint.distance_to(points[j]) < minDist:
@@ -151,32 +183,70 @@ func createWaypoints(nWps):
           failure = true
           break
   if !failure:
-#    removeSharpCorners()
     return bIdxs
-
+  print("Failure creating points, returning empty track indices.")
+  return PoolIntArray()
+  
+func circuitHasIntersections():
+  var bSize = bIdxs.size()
+  for bIdx in (bSize - 1):
+    var b1 = points[bIdxs[bIdx]]
+    var b2 = points[bIdxs[(bIdx + 1) % bSize]]
+    # Need to exclude the last segment for the first iteration
+    # since neighboring segments always overlap
+    var upperCIdx = bSize
+    if bIdx == 0:
+      upperCIdx -= 1 
+    for cIdx in range(bIdx + 2, upperCIdx):
+      var c1 = points[bIdxs[cIdx]]
+      var c2 = points[bIdxs[(cIdx + 1) % bSize]]
+      if (cIdx == bIdx || cIdx == (bIdx + 1) % bSize || (cIdx + 1) % bSize == bIdx):
+        print("something went wrong here, segment should not be subsequent.")
+      var intersection = Geometry.segment_intersects_segment_2d(b1, b2, c1, c2)
+      if (typeof(intersection) == TYPE_VECTOR2):
+        return true
+  return false
+  
+func segmentsPushPoints():
+  var movedSome = false
+  var bSize = bIdxs.size()
+  for bIdx in bSize:
+    var b1 = points[bIdxs[bIdx]]
+    var b2 = points[bIdxs[(bIdx + 1) % bSize]]
+    for pIdx in bSize:
+      if pIdx != bIdx && pIdx != (bIdx + 1) % bSize:
+        var point = points[bIdxs[pIdx]]
+        var closest = Geometry.get_closest_point_to_segment_2d(point, b1, b2)
+        var dist = (point - closest).length()
+        if dist < minDist:
+          # move point away from the segment
+          points[bIdxs[pIdx]] += minDist * (point - closest).normalized()
+          movedSome = true
+  return movedSome
 # For each sharp corner:
 # a. if possible, "cut" a small part off the corner, effectively turning the corner into an edge.
 # b. if there is no room because one of the neighbors is too close, simply remove the corner.
 func removeSharpCorners():
   var minSegment = 64
+  var removedSome = false
   var removedOne = true
   while bIdxs.size() > 2 and removedOne == true:
-    print("iteration!  bIdxs: " + str(bIdxs.size()))
+#    print("iteration!  bIdxs: " + str(bIdxs.size()))
     removedOne = false
-    for idx in range(bIdxs.size()):
+    for idx in bIdxs.size():
       var b = bIdxs[idx]
       var point = points[b]
       var prevP = points[bIdxs[(idx - 1 + bIdxs.size()) % bIdxs.size()]]
       var nextP = points[bIdxs[(idx + 1) % bIdxs.size()]]
       var angle = (prevP - point).angle_to(nextP - point)
-      if abs(angle) < PI / 8:
+      if abs(angle) < PI / 6.0:
         removedOne = true
+        removedSome = true
         offendingPoint = points[b]
         var vPrev = prevP - point
         var vNext = nextP - point
         # Calculate displacement based on angle
         var displacement = abs(0.5 * minSegment / sin(angle)) # sin angle = 0.5 * minSegment / displacement
-        print("displacement " + str(displacement))
         if displacement < min (vPrev.length(), vNext.length() - minSegment):
           var dir1 = vPrev.normalized()
           var dir2 = vNext.normalized()
@@ -188,11 +258,11 @@ func removeSharpCorners():
         else:
           points.remove(b)
           bIdxs.remove(idx)
-          for decreaseIdx in range(bIdxs.size()):
+          for decreaseIdx in bIdxs.size():
             if bIdxs[decreaseIdx] > b:
               bIdxs[decreaseIdx] -= 1
-        
         break # so that we restart the for-loop with a smaller number
+  return removedSome
 
 # Create the inside/outside of the track, using bisectors at each point.
 # side can be "left" or "right", which will correspond to the outside/inside
@@ -202,10 +272,10 @@ func wayPointsBorder(waypoints, side):
   var thisPoint
   var nextPoint
   var border = []
-  for wp in range (nWps):
-    prevPoint = waypoints[(wp + len(waypoints) - 1) % len(waypoints)]
-    thisPoint = waypoints[wp % len(waypoints)]
-    nextPoint = waypoints[(wp + 1) % len(waypoints)]
+  for wp in waypoints.size():
+    prevPoint = points[waypoints[(wp + len(waypoints) - 1) % len(waypoints)]]
+    thisPoint = points[waypoints[wp % len(waypoints)]]
+    nextPoint = points[waypoints[(wp + 1) % len(waypoints)]]
     var toNext = (nextPoint - thisPoint).normalized()
     var toPrev = (prevPoint - thisPoint).normalized()
     # Use cross product to decide which side to pick
@@ -229,18 +299,8 @@ func wayPointsBorder(waypoints, side):
 
 func _draw():
   if len(points) > 0:
+    for idx in bIdxs.size():
+      draw_line(points[bIdxs[idx]], points[bIdxs[(idx + 1) % bIdxs.size()]], Color(0.0, 1.0, 0.0))
     for point in points:
-      draw_circle(point, 4, Color(1, 0, 0))
-    for idx in len(bIdxs):
-      draw_line(points[bIdxs[idx]], points[bIdxs[(idx + 1) % len(bIdxs)]], Color(0.0, 1.0, 0.0))
-    if offendingPoint:
-      draw_circle(offendingPoint, 12, Color(1, 0, 0))
-#      draw_circle(offPrev, 8, Color(0, 1, 0))
-#      draw_circle(offNext, 8, Color(0, 0, 1))
-#    draw_circle(points[bIdxs[0]], 8, Color(0, 1, 0.5))
-#    for idx in triangCopy.size():
-#      var cor = Color(randf(), randf(), randf())
-#      draw_line(points[triangCopy[idx]], points[triangCopy[idx + 1]], cor)
-#      draw_line(points[triangCopy[idx + 1]], points[triangCopy[idx + 2]], cor)
-#      draw_line(points[triangCopy[idx + 2]], points[triangCopy[idx]], cor)
+      draw_circle(point, 10, Color(1, 0, 1))
     
--- Roadside.gd
+++ Roadside.gd
@@ -1,12 +1,19 @@
 extends Node2D
 
-var waypoints
+var points
+var pointOrder
 
-func _init(waypoints):
-  for idx in range(len(waypoints)):
-    var wp1 = waypoints[idx % len(waypoints)]
-    var wp2 = waypoints[(idx + 1) % len(waypoints)]
-    self.waypoints = waypoints
+func _init(thePoints, pIdxs = null):
+  points = thePoints
+  if pIdxs == null:
+    pointOrder = PoolIntArray()
+    for idx in range(points.size()):
+      pointOrder.push_back(idx)
+  else:
+    pointOrder = pIdxs
+  for idx in range(len(pointOrder)):
+    var wp1 = points[pointOrder[idx % len(pointOrder)]]
+    var wp2 = points[pointOrder[(idx + 1) % len(pointOrder)]]
     var staticShape = StaticBody2D.new()
     var collShape = CollisionShape2D.new()
     var lineSeg = SegmentShape2D.new()
@@ -15,14 +22,15 @@ func _init(waypoints):
     lineSeg.b = wp2
     rectSeg.set_extents((wp2 - wp1) / 2)
     collShape.shape = lineSeg
-#    collShape.position = wp1
     staticShape.add_child(collShape)
     staticShape.bounce = 0.5
     staticShape.friction = 0.5
     add_child(staticShape)
   
 func _draw():
-  for idx in range(len(waypoints)):
-    var wp1 = waypoints[idx % len(waypoints)]
-    var wp2 = waypoints[(idx + 1) % len(waypoints)]
-    draw_line(wp1, wp2, Color(255, 0, 0), 10)
+  for idx in range(len(pointOrder)):
+    var wp1 = points[pointOrder[idx % len(pointOrder)]]
+    var wp2 = points[pointOrder[(idx + 1) % len(pointOrder)]]
+    draw_line(wp1, wp2, Color(255, 0, 0), 5)
+  for point in points:
+      draw_circle(point, 10, Color(1, 1, 0))
