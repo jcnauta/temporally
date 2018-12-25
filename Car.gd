6dbba40e82635a993af8322809becd8e32932db9
Jelle Nauta <jelle.nauta@tno.nl>
2018-12-25 21:12:11 +0100

    Improved drifting

--- Car.gd
+++ Car.gd
@@ -23,6 +23,7 @@ var startRot
 func _ready():
   set_collision_mask_bit(7, true)
   set_collision_mask_bit(6, true)
+  a = 20000
 
 func control():
   pass
@@ -36,36 +37,45 @@ func _integrate_forces(state):
   var front = Vector2(1.0, 0.0).rotated(rotation)
   var velocity = get_linear_velocity()
   var frontSpeed = velocity.dot(front)
-  # longitudinal forces
-  var thrust_force = front * gas
-  var drag_force = (-0.001 * velocity.length() - 1.0) * velocity
-  # lateral forces
   var right = front.tangent()
-  var steering_force = velocity.dot(front) * turning * right
+  # determine drifting status
   var sideSpeed = abs(velocity.dot(right))
-  var lateral_drag = Vector2()
-  if sideSpeed > 300.0:
+  if sideSpeed > 200.0:
+    drifting = true
+    print("drifting")
+  elif sideSpeed < 0.1 || (gas == 0 && sideSpeed < 100.0):
     drifting = false
-  elif sideSpeed < 2.0:
-    drifting = false
+    
+  # longitudinal forces
+  var thrustForce
+  var dragForce
+  if drifting:
+    thrustForce = 0.5 * front * gas
+    dragForce = (-0.0001 * velocity.length() - 1.0) * velocity
+  else:
+    thrustForce = front * gas
+    dragForce = (-0.001 * velocity.length() - 1.0) * velocity
+  # lateral forces
+  var steeringForce = velocity.dot(front) * turning * right
+  var lateralDrag = Vector2()
   if !drifting:
-    lateral_drag = -100 * velocity.dot(right) * right
-  set_applied_force(thrust_force + drag_force + steering_force + lateral_drag)
+    lateralDrag = -100 * velocity.dot(right) * right
+  set_applied_force(thrustForce + dragForce + steeringForce + lateralDrag)
                         # how much are we steering
                         # sharper turns at low speed
-  var steering_torque
+  var steeringTorque
   if gas > 0:
-    steering_torque = -100 * turning * min(200, abs(frontSpeed))
+    steeringTorque = -100 * turning * min(200, abs(frontSpeed))
   elif gas < 0:
-    steering_torque = 100 * turning * min(200, abs(frontSpeed))
+    steeringTorque = 100 * turning * min(200, abs(frontSpeed))
   else:
-    steering_torque = -100 * turning * clamp(frontSpeed, -200, 200.0)
+    steeringTorque = -100 * turning * clamp(frontSpeed, -200, 200.0)
                         
   var phi = get_angular_velocity()
   var drag_torque = -3000 * phi * phi * sign(phi)
   if abs(get_angular_velocity()) > 3.0:
     drag_torque = -20000 * get_angular_velocity()
-  set_applied_torque(steering_torque + drag_torque)
+  set_applied_torque(steeringTorque + drag_torque)
   counter += 1
   
   if resetting:
